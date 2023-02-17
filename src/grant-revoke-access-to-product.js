9f0fc059a11641164984496c517555fb8674840e
Oscar Carrasquero <ozzambra@amazon.com>
2023-02-17 10:24:22 +0100

    Lambda functions updated to node18

--- src/grant-revoke-access-to-product.js
+++ src/grant-revoke-access-to-product.js
@@ -1,30 +1,41 @@
+const winston = require('winston');
 const AWS = require('aws-sdk');
 
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 const { SupportSNSArn: TopicArn } = process.env;
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
 
 
-exports.dynamodbStreamHandler = async (event) => {
+exports.dynamodbStreamHandler = async (event, context) => {
   await Promise.all(event.Records.map(async (record) => {
+    logger.defaultMeta = { requestId: context.awsRequestId };
+    logger.debug('event', { 'data': event });
+    logger.debug('context', { 'data': context });
     const oldImage = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage);
     const newImage = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
 
     // eslint-disable-next-line no-console
-    console.log(`DynamoDb record updated! OldImage: ${JSON.stringify(oldImage)} | NewImage: ${JSON.stringify(newImage)}`);
-
+    logger.debug('OldImage', { 'data': oldImage });
+    logger.debug('NewImage', { 'data': newImage });
     /*
       successfully_subscribed is set true:
         - for SaaS Contracts: no email is sent but after receiving the message in the subscription topic
         - for SaaS Subscriptions: after reciving the subscribe-success message in subscription-sqs.js
-
+  
       subscription_expired is set to true:
         - for SaaS Contracts: after detecting expired entitlement in entitlement-sqs.js
         - for SaaS Subscriptions: after reciving the unsubscribe-success message in subscription-sqs.js
     */
-    const grantAccess = newImage.successfully_subscribed === true && 
-      typeof newImage.is_free_trial_term_present !== "undefined" && 
-      ( oldImage.successfully_subscribed !== true || typeof oldImage.is_free_trial_term_present === "undefined" )
-      
+    const grantAccess = newImage.successfully_subscribed === true &&
+      typeof newImage.is_free_trial_term_present !== "undefined" &&
+      (oldImage.successfully_subscribed !== true || typeof oldImage.is_free_trial_term_present === "undefined")
+
 
     const revokeAccess = newImage.subscription_expired === true
       && !oldImage.subscription_expired;
@@ -34,6 +45,10 @@ exports.dynamodbStreamHandler = async (event) => {
     if (newImage.entitlement && oldImage.entitlement && (newImage.entitlement !== oldImage.entitlement)) {
       entitlementUpdated = true;
     }
+
+    logger.debug('grantAccess', { 'data': grantAccess });
+    logger.debug('revokeAccess:', { 'data': revokeAccess });
+    logger.debug('entitlementUpdated', { 'data': entitlementUpdated });
 
     if (grantAccess || revokeAccess || entitlementUpdated) {
       let message = '';
@@ -57,6 +72,8 @@ exports.dynamodbStreamHandler = async (event) => {
         Message: message,
       };
 
+      logger.info('Sending notification');
+      logger.debug('SNSparams', { 'data': SNSparams });
       await SNS.publish(SNSparams).promise();
     }
   }));
